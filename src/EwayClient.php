21a983ae7c5938d523b9fdb3c3fbed7606d25ce3
Ewan Thompson <ewanpt@gmail.com>
2019-04-30 17:53:04 +0800

    Start fleshing out class

--- src/EwayClient.php
+++ src/EwayClient.php
@@ -2,12 +2,16 @@
 
 namespace Ewan\Eway;
 
+use Ewan\Eway\Models\EwayCustomer;
 use Eway\Rapid\Client;
+use Eway\Rapid\Enum\ApiMethod;
+use Eway\Rapid\Model\Customer;
 
-class Eway
+class EwayClient
 {
 
     protected $client;
+    protected $ewayEnvironment;
 
     public function __construct(
         $ewayApiKey,
@@ -15,18 +19,56 @@ class Eway
         $ewayClientKey,
         $ewayEnvironment = 'sandbox'
     ) {
+        $this->ewayEnvironment = $ewayEnvironment;
         $this->client = new Client($ewayApiKey, $ewayApiPassword, $ewayEnvironment);
 
     }
 
-    public function createCustomer()
+    /**
+     * @param $attributes
+     */
+    public function createCustomer($attributes)
+    {
+        $customer = new EwayCustomer($attributes);
+        $customer = new Customer($attributes);
+        $request = [
+            'Customer' => $customer
+        ];
+        $result = $this->client->createCustomer(ApiMethod::DIRECT, $customer);
+        print_R($result);
+    }
+
+    /**
+     * @param string $ewayTokenCustomerID
+     *
+     * @return \Ewan\Eway\Models\EwayCustomer
+     */
+    public function readCustomer(string $ewayTokenCustomerID) : EwayCustomer
+    {
+        $customer = $this->client->queryCustomer($ewayTokenCustomerID);
+
+        if (!$customer->getErrors()) {
+            $customers = $customer->getAttribute('Customers');
+            /** @var \Eway\Rapid\Model\Customer $customer */
+            $customer = array_pop($customers);
+            return new EwayCustomer($customer->attributesToArray());
+        }
+
+        return null;
+    }
+
+    /**
+     * @param string $ewayTokenCustomerID
+     *
+     * @return array
+     */
+    public function transactions(string $ewayTokenCustomerID) : array
     {
 
     }
 
-    public function transactions($customerId)
+    private function modelToArray($model)
     {
-
     }
 
 }
